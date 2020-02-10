0c6b4884f32cf0853f468eec11f34aa5539ed7fd
AadilMehdi Sanchawala <aadilmehdis@gmail.com>
2020-02-10 22:15:34 +0530

    Adding decomposition code

--- Assignment1/q1.py
+++ Assignment1/q1.py
@@ -1,6 +1,23 @@
-from scipy import linalg as LA
+from scipy import linalg
 import numpy as np
 import random 
+
+def calculate_projection_error(P, world_coords, img_coords):
+    # Convert points into homogeneous coordinates
+    world_coords_homogeneous = np.concatenate((world_coords, np.ones((len(world_coords), 1))), axis=1)
+    image_coords_homogeneous = np.concatenate((img_coords, np.ones((len(img_coords), 1))), axis=1)
+
+    # Calculate the projected points and normalize them
+    projected_coords = P @ world_coords_homogeneous.T
+    projected_coords[0, :] = projected_coords[0, :] / projected_coords[2, :]
+    projected_coords[1, :] = projected_coords[1, :] / projected_coords[2, :]
+    projected_coords[2, :] = projected_coords[2, :] / projected_coords[2, :]
+    projected_coords = projected_coords.T
+
+    # Calculate the projection error
+    projection_error =  np.mean(np.sum(np.square(image_coords_homogeneous - projected_coords), axis=1))
+
+    return projection_error
 
 def construct_DLT_matrix(X, x):
     M = np.zeros((0, 12))
@@ -29,20 +46,8 @@ def RANSAC(world_coords, img_coords, max_iterations=5000):
         # Perform DLT and get the Transformation Matrix
         P = DLT(X, x)
 
-        # Convert points into homogeneous coordinates
-        world_coords_homogeneous = np.concatenate((world_coords, np.ones((len(world_coords), 1))), axis=1)
-        image_coords_homogeneous = np.concatenate((img_coords, np.ones((len(img_coords), 1))), axis=1)
-
-        # Calculate the projected points and normalize them
-        projected_coords = P @ world_coords_homogeneous.T
-        projected_coords[0, :] = projected_coords[0, :] / projected_coords[2, :]
-        projected_coords[1, :] = projected_coords[1, :] / projected_coords[2, :]
-        projected_coords[2, :] = projected_coords[2, :] / projected_coords[2, :]
-        projected_coords = projected_coords.T
-
-        # Calculate the projection error
-        projection_error =  np.mean(np.sum(np.square(image_coords_homogeneous - projected_coords), axis=1))
-
+        # Calculate projection error
+        projection_error = calculate_projection_error(P, world_coords, img_coords)
         # Check if projection error is lesser than the minimum projection error so far
         if projection_error < min_proj_error:
             min_proj_error = projection_error
@@ -67,8 +72,29 @@ def DLT(X, x):
 
     return P
 
-def QR():
-    pass
+def decompose_P(P):
+
+    # H = KR
+    H = P[:,0:3]
+
+    # h = -KRT
+    h = P[:,3]
+
+    # Get Translation vector
+    t = -np.linalg.inv(H)@h 
+
+    # Do QR decomposition of inv(H)
+    # This gives R.T and inv(K)
+    R, K = np.linalg.qr(np.linalg.inv(H))
+
+    # Obtain the R and K matrices
+    R = R.T
+    K = np.linalg.inv(K)
+
+    # Normalize K due to homogeneity
+    K = K / K[-1,-1]
+
+    return K, R, t
 
 def main():
     pass 
@@ -82,26 +108,22 @@ def main():
     P = RANSAC(world_coords, pixel_coords, max_iterations=50000)
 
     # Output the Tranformation Matrix
-
-    # Convert points into homogeneous coordinates
-    world_coords_homogeneous = np.concatenate((world_coords, np.ones((len(world_coords), 1))), axis=1)
-    image_coords_homogeneous = np.concatenate((pixel_coords, np.ones((len(pixel_coords), 1))), axis=1)
-
-    # Calculate the projected points and normalize them
-    projected_coords = P @ world_coords_homogeneous.T
-    projected_coords[0, :] = projected_coords[0, :] / projected_coords[2, :]
-    projected_coords[1, :] = projected_coords[1, :] / projected_coords[2, :]
-    projected_coords[2, :] = projected_coords[2, :] / projected_coords[2, :]
-    projected_coords = projected_coords.T
-
+    print("The Projection Matrix:")
     print(P)
-    print(projected_coords)
-    print(image_coords_homogeneous)
 
+    # Decompose Projection Matrix into Intrinsic, Rotation and Translation
+    K, R, t = decompose_P(P)
+    
+    print("Camera Intrinsic Matrix:")
+    print(K)
 
-    # Perform QR decomposition and get R and T
+    print("Camera Rotation Matrix:")
+    print(R)
 
-    # Output R and T
+    print("Camera Translation Vector:")
+    print(t)
+
+    print("Projection Error (MSE) for the Above Projection Matrix: {}".format(calculate_projection_error(P, world_coords, pixel_coords)))
 
 if __name__ == '__main__':
     main()
